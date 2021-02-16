611d9032f0fc2e629ea40b96aee5228b28a4c92b
bsvihovec <bsvihovec@yahoo.com>
2021-02-16 13:58:55 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,11 @@
 resource "null_resource" "operator-deploy" {
   provisioner "local-exec" {
       command = <<EOT
-        curl -s -X POST https://api.cloudability.com/v3/vendors/aws/accounts \
+        curl -s -X POST https://cm.globalcatalog.test.cloud.ibm.com/api/v1-beta/deploy/kubernetes/olm/operator \
              -H 'Content-Type: application/json' \
-             -u "$${CldAbltyAPIToken:?Missing Cloudability API Token Env Variable}:" \
-             -d '{"vendorAccountId": "${data.aws_caller_identity.current.account_id}", "type": "aws_role" }'
-      EOT
+             -H 'X-Auth-Refresh-Token: ${data.refresh_token}' \
+             -H 'Authorization: bearer ${data.token}' \
+             -d '{"cluster_id": "${data.cluster_id}", "region": "${data.region}", "all_namespaces": "${data.all_namespaces}", "version_locator_id": "${data.version_locator_id}", "namespaces": "${data.namespaces}"}'
+        EOT
   }
 }
